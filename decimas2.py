373572c7d2cdda043eab70704fdcd931321b6c1b
Nemefizz <107274943+Nemefizz@users.noreply.github.com>
2022-06-22 01:51:58 -0400

    Add files via upload

---
+++ decimas2.py
@@ -0,0 +1,20 @@
+import numpy as np
+from numpy import random
+A=random.randint(0,301,(10))
+B=random.randint(0,301,(10))
+print(A)
+print(B)
+print(f"La suma de los elementos del arreglo A es {A.sum()}")
+print(f"La suma de los elementos del arreglo B es {B.sum()}")
+sum=0
+for i in range(0,len(B)):
+    if B[i] % 2 != 0:
+        sum=sum+B[i]
+mul=0
+print(f"La suma de elementos impares es: {sum}")
+for i in range(0,len(B)):
+    if B[i] % 2 != 0:
+        mul=B[i]*B[i]*B[i]
+
+print(f"La multiplicacion de elementos impares es: {mul}")
+
